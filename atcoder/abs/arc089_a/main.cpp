513ad0690adae800d37805a17afded46f0c51672
t.saito <855739+takuma-saito@users.noreply.github.com>
2022-04-07 10:19:36 +0900

    abs 全部解いた

--- atcoder/abs/arc089_a/main.cpp
+++ atcoder/abs/arc089_a/main.cpp
@@ -5,11 +5,6 @@ typedef struct Pos {
     int t;
     int x;
     int y;
-    void Pos(int _t, int _x, int _y) {
-        t = _t;
-        x = _x;
-        y = _y;
-    }
 } Pos;
 vector<Pos> pos;
 
@@ -17,12 +12,19 @@ int main()
 {
     int N;
     cin >> N;
+    pos.push_back(Pos {0, 0, 0});
     rep(i, N) {
         int t, x, y;
         cin >> t >> x >> y;
-        pos.push_back(Pos(t, x, y));
+        pos.push_back(Pos {t, x, y});
     }
     sort(pos.begin(), pos.end(), [](Pos a, Pos b) {
         return a.t < b.t;
     });
+    for(int i=1; i<=N; i++) {
+        int v = abs(pos[i].x - pos[i-1].x) + abs(pos[i].y - pos[i-1].y);
+        int t = pos[i].t-pos[i-1].t;
+        if (t-v<0 || (t-v)&1) {cout << "No" << endl; return 0;}
+    }
+    cout << "Yes" << endl;
 }
