1b438d2308964a9c3f24cdf5c70f493195f1ba68
t.saito <855739+takuma-saito@users.noreply.github.com>
2022-04-14 23:02:21 +0900

    途中まで

--- aoj/courses/ALDS1/ALDS1_13_C/main.cpp
+++ aoj/courses/ALDS1/ALDS1_13_C/main.cpp
@@ -4,6 +4,7 @@ using namespace std;
 const int N = 4;
 const int NS = 16;
 typedef int B[N][N];
+int x[NS][2];
 struct State {
     int d;
     int t;
@@ -11,29 +12,37 @@ struct State {
     State() {
         this->d = 0;
         this->t = 0;
-        rep(i, 4) rep(j, 4) this->b[i][j] = 0;
+        rep(i, N) rep(j, N) this->b[i][j] = 0;
     }
     State(int d, int t, B b) {
         this->d = d;
         this->t = t;
-        rep(i, 4) rep(j, 4) this->b[i][j] = b[i][j];
+        rep(i, N) rep(j, N) this->b[i][j] = b[i][j];
+    }
+    void dump() {
+        rep(i, N) rep(j, N) printf("%d%s", b[i][j], j==N-1?"\n":" ");
     }
     bool operator<(const State& rhs) const
     {
-        if (d == rhs.d) return t < rhs.t;
+        if (d == rhs.d) t < rhs.t;
         return d < rhs.d;
     }
     bool operator>(const State& rhs) const
     {
-        if (d == rhs.d) return t > rhs.t;
+        if (d == rhs.d) t > rhs.t;
         return d > rhs.d;
+    }
+    bool operator==(const State& rhs) const
+    {
+        bool res=true;
+        rep(i, N) rep(j, N) res = res && (b[i][j] == rhs.b[i][j]);
+        return res;
     }
 };
 int d[4][2] = {{1, 0}, {-1, 0}, {0, -1}, {0, 1}};
 
 int dist(B& b) {
-    int x[NS][2], y[NS][2];
-    rep(i, NS) { x[i][0]=i/N; x[i][1]=i%N; }
+    int y[NS][2];
     rep(i, N) rep(j, N) { int u = b[i][j]; y[u][0] = i; y[u][1] = j; }
     int sum = 0;
     rep(i, NS) sum += abs(x[i][0]-y[i][0]) + abs(x[i][1]-y[i][1]);
@@ -42,17 +51,24 @@ int dist(B& b) {
 
 int main()
 {
+    map<State, bool> used;
     State s; rep(i, N) rep(j, N) cin >> s.b[i][j];
     priority_queue<State, vector<State>, greater<State>> pq;
+    rep(i, NS-1) { x[i+1][0]=i/N; x[i+1][1]=i%N; }
+    x[0][0]=N-1; x[0][1]=N-1;
     s.d = dist(s.b);
     pq.push(s);
     while (!pq.empty()) {
         auto s = pq.top(); pq.pop();
-        cout << s.d << " " << s.t << endl;
         if (s.d==0) {
             printf("%d\n", s.t);
             break;
         }
+        s.dump();
+        cout << s.d << " " << s.t << endl;
+        cout << endl;
+        //if (used.count(s)) continue;
+        //used[s] = true;
         int x = -1, y = -1;
         rep(i, N) rep(j, N) if(s.b[i][j]==0) {x=i; y=j;}
         if (x==-1||y==-1) runtime_error("wrong");
