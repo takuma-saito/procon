78b30f042c484cd78971c7fc724462a7934101fa
t.saito <855739+takuma-saito@users.noreply.github.com>
2022-04-23 23:24:18 +0900

    add abc150

---
+++ atcoder/abc/abc150/abc150_c/main.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+const int MAX_N = 9;
+int qnum, pnum, N, P[MAX_N], Q[MAX_N], used[MAX_N];
+
+bool solve(int depth, int k, int& num, bool is_q, bool is_p) {
+    if (depth==N) {
+        num++;
+        if (is_p) qnum=num;
+        if (is_q) pnum=num;
+        return qnum>0&&pnum>0;
+    }
+    for(int i=0; i<N; i++) {
+        if (used[i]) continue;
+        used[i] = true;
+        if (solve(depth+1, k+1, num, is_q && (Q[k] == i+1), is_p && (P[k] == i+1))) return true;
+        used[i] = false;
+    }
+    return false;
+}
+
+int main()
+{
+    cin >> N;
+    rep(i, N) cin >> P[i];
+    rep(i, N) cin >> Q[i];
+    int num=0;
+    solve(0, 0, num, true, true);
+    printf("%d\n", abs(qnum-pnum));
+}
