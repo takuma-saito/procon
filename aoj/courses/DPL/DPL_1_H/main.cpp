d0d21fab6edde7c5e692963a51674b6b8f381f46
t.saito <855739+takuma-saito@users.noreply.github.com>
2022-04-26 10:19:23 +0900

    半分列挙難しい

--- aoj/courses/DPL/DPL_1_H/main.cpp
+++ aoj/courses/DPL/DPL_1_H/main.cpp
@@ -3,12 +3,15 @@ using namespace std;
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
 typedef long long ll;
 typedef pair<ll, ll> P;
-const int MAX_N = 20;
+const int MAX_N = 40;
+const ll INF = 1e18;
 ll N, W; ll weight[MAX_N]; ll value[MAX_N];
+
+// TODO: https://onlinejudge.u-aizu.ac.jp/solutions/problem/DPL_1_H/review/4968199/emtsu_ba/C++17
 
 int main()
 {
-    vector<P> vec, vec2;
+    vector<P> vec;
     fill(weight, weight+MAX_N, 0);
     fill(value, value+MAX_N, 0);
 
@@ -16,6 +19,7 @@ int main()
     rep(i, N) cin >> value[i] >> weight[i];
     int D = N/2;
 
+    vec.push_back(P{0, 0});
     for(int i=1; i<(1<<D); i++) {
         ll sw=0, sv=0; for(int j=0; j<D; j++) if (i>>j&1) {
             sw += weight[j];
@@ -23,9 +27,11 @@ int main()
         }
         vec.push_back({sw, sv});
     }
-    sort(vec.begin(), vec.end(), [](const P& l, const P& r) {
-        return l.first == r.first ? (l.second > r.second) : (l.second < r.second);
-    });
+    sort(vec.begin(), vec.end());
+
+    int m = 1; for(int i=1; i<vec.size(); i++) {
+        if (vec[m-1].second < vec[i].second) vec[m++] = vec[i];
+    }
 
     int G = N-D;
     ll ans = 0;
@@ -34,10 +40,9 @@ int main()
             sw += weight[D+j];
             sv += value[D+j];
         }
-        auto it = lower_bound(vec.begin(), vec.end(),
-            P{W-sw, -1}, [](const P& l, const P& r){ return l.first < r.first; });
-        int idx = distance(vec.begin(), it);
-        ans = max(ans, sv+vec[idx].second);
+        if (W-sw<0) continue;
+        auto it = lower_bound(vec.begin(), vec.begin()+m, P{W-sw, INF});
+        ans = max(ans, (it == vec.begin()) ? sv : sv + (--it)->second);
     }
 
     printf("%lld\n", ans);
